6133b126f5269a825279f7b4053eb6d498b93fcf
bazanks039@gmail.com <bazanks039@gmail.com>
2024-06-25 19:29:56 +0300

    27.1

---
+++ 27.1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Укажите количество рублей: ");
+        decimal roubles = decimal.Parse(Console.ReadLine());
+
+        decimal roublesToTenge = RubleConverter.ToTenge(roubles);
+        Console.WriteLine($"В тенге: {roubles}");
+
+        decimal roublesToDollars = RubleConverter.ToDollars(roubles);
+        Console.WriteLine($"В долларах США: {roubles}");
+
+        decimal roublesToEuros = RubleConverter.ToEuros(roubles);
+        Console.WriteLine($"В евро: {roubles}");
+
+        decimal roublesToZlotys = RubleConverter.ToZlotys(roubles);
+        Console.WriteLine($"В злотах: {roubles}");
+
+        decimal roublesToYuan = RubleConverter.ToYuan(roubles);
+        Console.WriteLine($"В юань: {roubles}");
+
+        decimal roublesToRupes = RubleConverter.ToRupes(roubles);
+        Console.WriteLine($"В рупий: {roubles}");
+    }
+}
+
+class RubleConverter
+{
+    public static decimal ToTenge(decimal rubles)
+    {
+        return rubles * 5.23m;
+    }
+
+    public static decimal ToDollars(decimal rubles)
+    {
+        return rubles * 0.011369m;
+    }
+
+    public static decimal ToEuros(decimal rubles)
+    {
+        return rubles * 0.010609m;
+    }
+
+    public static decimal ToZlotys(decimal rubles)
+    {
+        return rubles * 0.046075m;
+    }
+
+    public static decimal ToYuan(decimal rubles)
+    {
+        return rubles * 0.083019m;
+    }
+
+    public static decimal ToRupes(decimal rubles)
+    {
+        return rubles * 0.950281m;
+    }
+
+}
